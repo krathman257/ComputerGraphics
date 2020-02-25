0889ab18eae21661b5754a1b91113fb8ed783c92
Kyle Rathman <krathman257@gmail.com>
2020-02-25 11:46:56 -0700

    Finished Lab 5

--- labs/lab5/main.cpp
+++ labs/lab5/main.cpp
@@ -7,15 +7,18 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <csci441/shader.h>
-#include <csci441/matrix4.h>
-#include <csci441/vector4.h>
-#include <csci441/uniform.h>
-
-#include "shape.h"
+#include "csci441/Shader.h"
+#include "csci441/Matrix4.h"
+#include "csci441/Vector4.h"
+#include "csci441/Vector3.h"
+#include "csci441/Uniform.h"
+#include "csci441/Shape.h"
 
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 960;
+
+bool modeSwitch = false;
+int mode = 0;
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -34,7 +37,7 @@ Matrix4 processModel(const Matrix4& model, GLFWwindow *window) {
 
     const float ROT = 1;
     const float SCALE = .05;
-    const float TRANS = .01;
+    const float TRANS = .025;
 
     // ROTATE
     if (isPressed(window, GLFW_KEY_U)) { trans.rotate_x(-ROT); }
@@ -49,10 +52,13 @@ Matrix4 processModel(const Matrix4& model, GLFWwindow *window) {
     // TRANSLATE
     else if (isPressed(window, GLFW_KEY_UP)) { trans.translate(0, TRANS, 0); }
     else if (isPressed(window, GLFW_KEY_DOWN)) { trans.translate(0, -TRANS, 0); }
-    else if (isPressed(window, GLFW_KEY_LEFT)) { trans.translate(-TRANS, 0, 0); }
-    else if (isPressed(window, GLFW_KEY_RIGHT)) { trans.translate(TRANS, 0, 0); }
+    else if (isPressed(window, GLFW_KEY_LEFT)) { trans.translate(TRANS, 0, 0); }
+    else if (isPressed(window, GLFW_KEY_RIGHT)) { trans.translate(-TRANS, 0, 0); }
     else if (isPressed(window, ',')) { trans.translate(0,0,TRANS); }
     else if (isPressed(window, '.')) { trans.translate(0,0,-TRANS); }
+    // MODE
+    else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) { modeSwitch = true; }
+    else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE && modeSwitch) { mode = (mode + 1) % 2; modeSwitch = false; }
 
     return trans * model;
 }
@@ -103,29 +109,48 @@ int main(void) {
 
 
     // create c
-    // Cylinder c(20, 1, .2, .4);
+    Cylinder cylinder(400, 1, .2, .4);
     // Cone c(20, 1, .2, .4);
     // Sphere c(20, .5, 1, .2, .4);
     // Torus c(20, .75, .25, 1, .2, .4);
-    DiscoCube c;
+    DiscoCube cube;
 
     // copy vertex data
-    GLuint VBO1;
-    glGenBuffers(1, &VBO1);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-    glBufferData(GL_ARRAY_BUFFER, c.coords.size()*sizeof(float),
-            &c.coords[0], GL_STATIC_DRAW);
-
-    // describe vertex layout
-    GLuint VAO1;
-    glGenVertexArrays(1, &VAO1);
-    glBindVertexArray(VAO1);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float),
-            (void*)0);
+    GLuint VBO_CYL;
+    glGenBuffers(1, &VBO_CYL);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_CYL);
+    glBufferData(GL_ARRAY_BUFFER, cylinder.coords.size()*sizeof(float),
+            &cylinder.coords[0], GL_STATIC_DRAW);
+    GLuint VAO_CYL;
+    glGenVertexArrays(1, &VAO_CYL);
+    glBindVertexArray(VAO_CYL);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float),
+        (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float),
-            (void*)(3*sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float),
+        (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float),
+        (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    GLuint VBO_CUBE;
+    glGenBuffers(1, &VBO_CUBE);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_CUBE);
+    glBufferData(GL_ARRAY_BUFFER, cube.coords.size()*sizeof(float),
+            &cube.coords[0], GL_STATIC_DRAW);
+    GLuint VAO_CUBE;
+    glGenVertexArrays(1, &VAO_CUBE);
+    glBindVertexArray(VAO_CUBE);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float),
+        (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float),
+        (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float),
+        (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // setup projection
     Matrix4 projection;
@@ -140,13 +165,19 @@ int main(void) {
     camera.look_at(eye, origin, up);
 
     // create the shaders
-    Shader shader("../vert.glsl", "../frag.glsl");
+    Shader shader("vert.glsl", "frag.glsl");
 
     // setup the textures
     shader.use();
 
     // set the matrices
     Matrix4 model;
+    Matrix3 normalTransform;
+    Vector4 lightPosition(-3.0f, -3.0f, 3.0f, 1.0f);
+    Vector4 lightColor(1.0f, 1.0f, 0.0f, 1.0f);
+    float lightIntensity = 2,
+          specular = 32,
+          ambient = 0.2;
 
     // and use z-buffering
     glEnable(GL_DEPTH_TEST);
@@ -155,6 +186,7 @@ int main(void) {
     while (!glfwWindowShouldClose(window)) {
         // process input
         processInput(model, window);
+        normalTransform.inverse_transpose(projection * camera * model);
 
         /* Render here */
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -166,10 +198,33 @@ int main(void) {
         Uniform::set(shader.id(), "model", model);
         Uniform::set(shader.id(), "projection", projection);
         Uniform::set(shader.id(), "camera", camera);
+        Uniform::set(shader.id(), "normTransform", normalTransform);
+        Uniform::set(shader.id(), "lightPos", lightPosition);
+        Uniform::set(shader.id(), "lightColor", lightColor);
+        Uniform::set(shader.id(), "lightIntensity", lightIntensity);
+        Uniform::set(shader.id(), "specular", specular);
+        Uniform::set(shader.id(), "ambient", ambient);
+
+        float size;
+
+        switch (mode) {
+            case 0:
+                glBindBuffer(GL_ARRAY_BUFFER, VBO_CUBE);
+                size = cube.coords.size() * sizeof(float);
+                glBindVertexArray(VAO_CUBE);
+                break;
+            case 1:
+                glBindBuffer(GL_ARRAY_BUFFER, VBO_CYL);
+                size = cylinder.coords.size() * sizeof(float);
+                glBindVertexArray(VAO_CYL);
+                break;
+            default:
+                break;
+        }
 
         // render the cube
-        glBindVertexArray(VAO1);
-        glDrawArrays(GL_TRIANGLES, 0, c.coords.size()*sizeof(float));
+        
+        glDrawArrays(GL_TRIANGLES, 0, size);
 
         /* Swap front and back and poll for io events */
         glfwSwapBuffers(window);
