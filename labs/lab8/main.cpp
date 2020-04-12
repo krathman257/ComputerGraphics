373bd51bf2d5c06a9d80e9e07b1b90884a93537c
Kyle Rathman <krathman257@gmail.com>
2020-04-12 15:19:51 -0600

    Finished Lab 8

--- labs/lab8/main.cpp
+++ labs/lab8/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-
 #include <glm/glm.hpp>
-
 #include <bitmap/bitmap_image.hpp>
+
+const float MAXVAL = std::numeric_limits<float>::max();
 
 struct Viewport {
     glm::vec2 min;
@@ -14,22 +14,92 @@ struct Viewport {
 
 struct Sphere {
     int id;
-    glm::vec3 center;
-    glm::vec3 color;
+    glm::vec3 origin;
+    rgb_t color;
     float radius;
 
-    Sphere(const glm::vec3& center=glm::vec3(0,0,0),
-            float radius=0,
-            const glm::vec3& color=glm::vec3(0,0,0))
-        : center(center), radius(radius), color(color) {
+    Sphere(const glm::vec3& o=glm::vec3(0,0,0),
+            float r=0,
+            const glm::vec3& c=glm::vec3(0,0,0))
+        : origin(o), radius(r) {
+            color = make_colour(c.x*255, c.y*255, c.z*255);
             static int id_seed = 0;
             id = ++id_seed;
         }
 };
 
+struct Ray {
+    glm::vec3 origin;
+    glm::vec3 direction;
 
-void render(bitmap_image& image, const std::vector<Sphere>& world) {
-    // TODO: implement ray tracer
+    Ray(const glm::vec3& origin = glm::vec3(0, 0, 0),
+        const glm::vec3& direction = glm::vec3(0, 0, 0))
+        : origin(origin), direction(normalize(direction)) {}
+
+    float findNearestIntersection(Sphere s) {
+        float a = dot(direction, direction);
+        float b = dot(2.0f * direction, origin - s.origin);
+        float c = dot(origin - s.origin, origin - s.origin) - s.radius * s.radius;
+
+        float d = b * b - 4 * a * c;
+
+        if (d < 0) {
+            return MAXVAL;
+        }
+        else if (d == 0) {
+            return (-1 * b) / (2 * a);
+        }
+        else if (d > 0) {
+            float r1 = (-1 * b + sqrt(d)) / (2 * a);
+            float r2 = (-1 * b - sqrt(d)) / (2 * a);
+            return std::min(r1, r2);
+        }
+    }
+};
+
+void render(bitmap_image& image, const std::vector<Sphere>& world, int mode=0, float perspDist=1) {
+
+    Viewport view = Viewport(glm::vec2(-5, -5), glm::vec2(5, 5));
+    rgb_t background = make_colour(75, 156, 211);
+    float w = image.width(), h = image.height();
+
+    for (int i = 0; i < image.width(); i++) {
+        for (int j = 0; j < image.height(); j++) {
+            image.set_pixel(i, h-j-1, background);
+
+            glm::vec3 o, d;
+            float ui = view.min[0] + (view.max[0] - view.min[0]) * (i + 0.5f) / image.width();
+            float vj = view.min[1] + (view.max[1] - view.min[1]) * (j + 0.5f) / image.height();
+
+            switch (mode) {
+            case 0:
+                o = glm::vec3(ui, vj, 0);
+                d = glm::vec3(0, 0, 1);
+                break;
+
+            case 1:
+                o = glm::vec3(0, 0, -perspDist);
+                d = glm::vec3(ui, vj, perspDist);
+                break;
+
+            default:
+                break;
+            }
+
+            Ray r = Ray(o, d);
+            
+            float lowestT = MAXVAL;
+
+            for (int k = 0; k < world.size(); k++) {
+                Sphere tempSphere = world[k];
+                float ni = r.findNearestIntersection(tempSphere);
+                if (ni < lowestT) {
+                    lowestT = ni;
+                    image.set_pixel(i, h-j-1, tempSphere.color);
+                }
+            }
+        }
+    }
 }
 
 int main(int argc, char** argv) {
@@ -45,10 +115,11 @@ int main(int argc, char** argv) {
     };
 
     // render the world
-    render(image, world);
+    render(image, world, 0);
+    image.save_image("orthographic.bmp");
 
-    image.save_image("ray-traced.bmp");
+    render(image, world, 1, 5);
+    image.save_image("perspective.bmp");
+
     std::cout << "Success" << std::endl;
 }
-
-
